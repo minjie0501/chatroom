26b4185489bac7552af1e8d7451abfa089f4ccd8
minjie0501 <minjie.fan@icloud.com>
2021-12-02 17:07:01 +0100

    cleaned the code and added most functionlaities

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import http from "http";
 import { fileURLToPath } from "url";
-import path, { dirname } from "path";
+import { dirname } from "path";
 import { Server } from "socket.io";
 
 const PORT = 3000;
@@ -15,31 +15,36 @@ const server = http.createServer(app);
 
 const io = new Server(server, {});
 
-app.use("/", express.static(clientPath));
-
-let users = [];
-let counter = 0
-
 io.on("connection", (socket) => {
-  users.push({ id: socket.id, name: `name:${counter}` });
-  counter++
-  io.emit("users", users);
-
+  console.log(socket.id);
   socket.on("sendToAll", (message, user) => {
-    io.emit("displayMessage", message, user);
+    /// listening to sendToAll events
+    io.emit("displayMessage", message, user); //send displayMessage event to everyone
   });
 
+  // Send message just to me
   socket.on("sendToMe", (message) => {
-    socket.emit("sendToMe", message);
+    socket.emit("displayToMe", message);
   });
 
+  // Send private message
+  socket.on("sendPrivate", (message, user, sendTo) => {
+    socket.to(sendTo).emit("displayPrivateMessage", message, user);
+  });
+
+  // Show all clients for all users
+  io.emit("clients", Array.from(io.sockets.sockets.keys()));
+
+  // Display client id
+  socket.emit("displayClientId", socket.id);
+
+  // Remove client from all clients when someone disconnects
   socket.on("disconnect", () => {
-    users = users.filter(function (obj) {
-      return obj.id !== socket.id;
-    });
-    console.log(users);
+    io.emit("clients", Array.from(io.sockets.sockets.keys()));
   });
 });
+
+app.use("/", express.static(clientPath));
 
 server.listen(PORT, () => {
   console.log("server running on " + PORT);
